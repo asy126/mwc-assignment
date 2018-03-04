4f20b7c3cd3c21a1b7a8f9dbfac8084d4e58c592
Haji zainul Abidin <nws05_01094142@pb.edu.bn>
2018-03-04 17:40:08 +0800

    fire base masih inda mau, cuba tukarkn

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {MenuPage} from '../menu/menu';
+import { detailListServer } from '../../server/detail-lists/detail-lists.server';
+import { Observable } from 'rxjs/Observable';
+import { Detail } from '../../Login/details/details.Login';
 // import {ListPage} from '../list/list';
 // import {ContentPage} from '../content/content';
 
@@ -12,12 +15,24 @@ import {MenuPage} from '../menu/menu';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  detailList$: Observable<Detail[]>
 
+  constructor(public navCtrl: NavController, private list: detailListServer) {
+    this.detailList$ = this.list
+      .getDetailList() //DB list
+      .snapshotChanges()
+      .map(
+        changes => {
+          return changes.map(c => ({
+            key: c.payload.key, ...c.payload.val()
+          }))
+        }
+      )
 }
   showmenu() {
     this.navCtrl.push(MenuPage);
 }
+
 
 // showlist() {
 //   this.navCtrl.push(ListPage);
